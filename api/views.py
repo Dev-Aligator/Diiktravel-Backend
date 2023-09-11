7ebbe0e7d0c558d65369fe02d8c2987e907fbec1
Dev-Aligator <devaligator1004@gmail.com>
2023-09-11 22:08:53 +0700

    Perform distance calculation and keyword matching to get suitable places

--- api/views.py
+++ api/views.py
@@ -2,36 +2,95 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from base.models import Place
 from .serializers import PlaceSerializer
-import random
+import json
 from thefuzz import fuzz
+import math
 @api_view(['GET'])
 def getPlaceData(request):
-    page = request.GET.get('page', 1)
-    page = int(page)
-    keyword = request.GET.get('keyword', 1)
+    # Get query parameters
+    page = int(request.GET.get('page', 1))
+    keyword = request.GET.get('keyword', '')
+    location = request.GET.get('location', '0.0,0.0')
+    
+    # Parse latitude and longitude from location string
+    location_parts = location.split(',')
+    user_latitude = float(location_parts[0])
+    user_longitude = float(location_parts[1])
+    # Calculate pagination indexes
+    items_per_page = 12
+    start_index = (page - 1) * items_per_page
+    end_index = page * items_per_page
 
-    similarity_threshold = 50
-    # write new code
-    startIndex = 12*(page-1)
-    endIndex = 12*page
-    all_places = Place.objects.all()
+    places = Place.objects.all()
 
-    if keyword:
-        filtered_places = [
-            (place, fuzz.ratio(keyword, place.name)) for place in all_places if fuzz.ratio(keyword, place.name) >= similarity_threshold
-        ]
-        sorted_filtered_places = sorted(filtered_places, key=lambda x: x[1], reverse=True)
-        filtered_places = [place for place, _ in sorted_filtered_places]
+    # Apply keyword filtering
 
-    else:
-        filtered_places = all_places
 
-    total_filtered_places = len(filtered_places)
-    places = filtered_places[startIndex:endIndex]
-    serializer = PlaceSerializer(places, many=True)
+    filtered_places = []
+
+    for place in places:
+        # Calculate the matching score using fuzzywuzzy's fuzz.ratio function
+        matching_score = 100
+        if keyword:
+            matching_score = fuzz.ratio(keyword, place.name)
+            if matching_score < 40:
+                continue
+        # Calculate the distance between the user and the place
+        place_distance = getDistance(
+            user_latitude,
+            user_longitude,
+            json.loads(place.location)['lat'],
+            json.loads(place.location)['lng']
+        )
+        place.distance_to_user = place_distance
+        
+        # Calculate the place score as matching score divided by distance
+        place_score = ( matching_score**1.4 ) / place_distance
+        # Append the place and its score to the list
+        filtered_places.append((place, place_score))
+
+    # Sort the places based on the place score in descending order
+    sorted_filtered_places = sorted(filtered_places, key=lambda x: x[1], reverse=True)
+    # Extract the sorted places
+    sorted_places = [place for place, score in sorted_filtered_places]
+    # Calculate the total filtered places
+    total_filtered_places = len(sorted_places)
+
+    # Apply ordering by distance
+
+    # Apply pagination
+    paginated_places = sorted_places[start_index:end_index]
+
+    # Serialize the results
+    serializer = PlaceSerializer(paginated_places, many=True)
+    
+    # Create a response dictionary
     response_data = {
         'places': serializer.data,
-        'total_filtered_places': total_filtered_places
+        'total_filtered_places': total_filtered_places,
     }
+    
     return Response(response_data)
 
+
+def getDistance(lat1, lng1, lat2, lng2):
+    # Radius of the Earth in kilometers
+    earth_radius = 6371.0
+
+    # Convert latitude and longitude from degrees to radians
+    lat1 = math.radians(lat1)
+    lng1 = math.radians(lng1)
+    lat2 = math.radians(lat2)
+    lng2 = math.radians(lng2)
+
+    # Haversine formula
+    dlat = lat2 - lat1
+    dlng = lng2 - lng1
+
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlng / 2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    # Calculate the distance
+    distance = earth_radius * c
+
+    return distance
--- base/models.py
+++ base/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-
 # Create your models here.
 
 
@@ -15,47 +14,5 @@ class Place(models.Model):
     totalRating = models.FloatField(null=True,blank=True)
     photo = models.CharField(max_length=200, null=True, blank=True)
 
-# class Banner(models.Model):
-#     tagLine = models.CharField(max_length=200, default="Welcome to my Portfolio")
-#     plainText = models.CharField(max_length=500)
-#     created = models.DateTimeField(auto_now_add=True)
-
-# class Role(models.Model):
-#     roleName = models.CharField(max_length=200)
-#     created = models.DateTimeField(auto_now_add=True)
-#     banner = models.ForeignKey(Banner, on_delete=models.CASCADE, related_name="roles")
-
-#     def __str__(self):
-#         return self.roleName
-
-# class Skill(models.Model):
-#     skillPercentage = models.PositiveSmallIntegerField(default=90)
-#     stopColor1 = models.CharField(max_length=20, null=True, blank=True)
-#     stopColor2 = models.CharField(max_length=20, null=True, blank=True)    
-#     skillName = models.CharField(max_length=100, unique=True)
-
-#     def __str__(self):
-#         return self.skillName
-
-# class Project(models.Model):
-#     title = models.CharField(max_length=200, unique=True)
-#     description = models.CharField(max_length=200)
-#     imgUrl = models.ImageField(upload_to="images/")
-#     tabIndex = models.PositiveSmallIntegerField(default=1)
-    
-#     def __str__(self):
-#         return self.title
-
-# class Contact(models.Model):
-#     firstName = models.CharField(max_length=200)
-#     lastName = models.CharField(max_length=200)
-#     email = models.EmailField(max_length=50)
-#     phone = models.CharField(max_length=20, null=True, blank=True)
-#     message = models.CharField(max_length=500)
-#     created = modles.DateTimeField(auto_now_add=true)
-
-#     def ___str__(self):
-#         return self.firstName + self.lastName
-
-    
+    distance_to_user = models.FloatField(null=True, blank=True)
 
